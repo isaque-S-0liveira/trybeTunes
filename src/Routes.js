0d07979e12bf1091962e599bece8dad9f5c4fdb4
Isaque <zazac3179@gmail.com>
2022-11-03 17:23:35 -0300

    req 2

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -11,15 +11,15 @@ import Search from './pages/Search';
 class Routes extends React.Component {
   render() {
     return (
-      <>
+      <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="" component={ NotFound } />
-      </>
+        <Route path="/profile" component={ Profile } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../Components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bttHabillit: true,
+      inputValue: '',
+      loading: false,
+      redirect: false,
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    const minValue = 3;
+    if (value.length >= minValue) {
+      this.setState({
+        bttHabillit: false,
+        inputValue: value,
+      });
+    } else {
+      this.setState({ bttHabillit: true });
+    }
+  };
+
+  handleClick = async () => {
+    const { inputValue } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: inputValue });
+    this.setState({
+      loading: false,
+      redirect: true,
+    });
+  };
+
   render() {
+    const { bttHabillit, loading, redirect } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <>
+        { redirect && <Redirect to="/search" /> }
+        {loading ? <Loading /> : (
+          <div data-testid="page-login">
+            <form>
+              <input
+                data-testid="login-name-input"
+                type="text"
+                placeholder="Nome"
+                onChange={ this.handleChange }
+              />
+              <button
+                data-testid="login-submit-button"
+                type="button"
+                disabled={ bttHabillit }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </form>
+          </div>
+        )}
+      </>
     );
   }
 }
