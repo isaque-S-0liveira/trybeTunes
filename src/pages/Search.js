bff513a9dc04656ee5b46376476b5c2713af3ca4
Isaque <zazac3179@gmail.com>
2022-11-03 18:20:03 -0300

    req 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      disabledBtt: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    const minValue = 2;
+    if (value.length >= minValue) {
+      this.setState({
+        disabledBtt: false,
+      });
+    } else {
+      this.setState({ disabledBtt: true });
+    }
+  };
+
   render() {
+    const { disabledBtt } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Pesquisar artista ou banda"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ disabledBtt }
+          >
+            Pesquisar
+
+          </button>
+        </form>
       </div>
     );
   }
